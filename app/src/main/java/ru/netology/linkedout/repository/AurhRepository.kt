c1166d2dae4a829b70f3bf1f14b52b4b3566cc07
Albert R. Burnyashov <albrburn@gmail.com>
2024-02-19 21:43:45 +0300

    Modified AuthRepository, added EventRemoteMediator

--- app/src/main/java/ru/netology/linkedout/repository/AurhRepository.kt
+++ app/src/main/java/ru/netology/linkedout/repository/AurhRepository.kt
@@ -1,5 +1,19 @@
 package ru.netology.linkedout.repository
 
+import android.util.Log
+import kotlinx.coroutines.CancellationException
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import ru.netology.linkedout.apiservice.ApiService
+import ru.netology.linkedout.auth.AuthState
+import ru.netology.linkedout.dto.MediaUpload
+import ru.netology.linkedout.error.ApiError
+import ru.netology.linkedout.error.NetworkError
+import java.io.IOException
+import javax.inject.Inject
+
 interface AuthRepository {
     suspend fun signIn(
         login: String,
@@ -19,4 +33,70 @@ interface AuthRepository {
         media: MediaUpload,
     ): AuthState
 
+}
+
+class AuthRepositoryImpl @Inject constructor(
+    private val apiService: ApiService,
+) : AuthRepository {
+
+    override suspend fun signIn(login: String, pass: String): AuthState {
+        try {
+            val response = apiService.authenticateUser(login, pass)
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+            return response.body() ?: throw ApiError(response.code(), response.message())
+        } catch (e: IOException) {
+            throw NetworkError
+        } catch (e: Exception) {
+            throw NetworkError
+        }
+    }
+
+    override suspend fun registerNewUser(login: String, pass: String, name: String): AuthState {
+
+        try {
+
+            val response = apiService.registerUser(login, pass, name)
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+            return response.body() ?: throw ApiError(response.code(), response.message())
+        } catch (e: IOException) {
+            throw NetworkError
+        } catch (e : CancellationException) {
+            throw CancellationException()
+        }
+        catch (e: Exception) {
+            throw NetworkError
+        }
+    }
+
+    override suspend fun registerWithPhoto(
+        login: String,
+        pass: String,
+        name: String,
+        media: MediaUpload
+    ): AuthState {
+        try {
+            val file = MultipartBody.Part.createFormData(
+                "file", media.file.name, media.file.asRequestBody()
+            )
+            val response = apiService.registerWithPhoto(
+                login.toRequestBody("text/plain".toMediaType()),
+                pass.toRequestBody("text/plain".toMediaType()),
+                name.toRequestBody("text/plain".toMediaType()),
+                file
+            )
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+            return response.body() ?: throw ApiError(response.code(), response.message())
+        } catch (e: IOException) {
+            throw NetworkError
+        } catch (e: Exception) {
+            Log.e("error", e.message.toString())
+            throw NetworkError
+        }
+    }
 }
